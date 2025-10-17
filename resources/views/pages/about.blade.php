aab689b34f85080ae4b40d0fe02018fb7f816b8b
sendyprayogo <sendyprayogo@MacBook-Pro.local>
2025-10-17 13:03:41 +0700

    rubah ke modul 6

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
 
 @section('content')
-<h1>Selamat datang di halaman tentang kami</h1>
+<h1>Selamat datang!! <br> Perkanalkan saya {{$nama}}, dari kelas {{$kelas}} dengan nim {{$nim}}</h1>
 @endsection
